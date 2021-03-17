1ef5b1f6af222a5c2514aa94ff2deebc7172e5e7
Kim-777 <iloo3012@naver.com>
2021-03-17 10:46:01 +0900

    2021-3-17

--- 팩토리얼.js
+++ 팩토리얼.js
@@ -30,4 +30,16 @@ function solution(N) {
 
 }
 
+function solution2(n) {
+    let answer;
+
+    function DFS(n) {
+        if(n===1) return 1;
+        else return n * DFS(n-1);
+    }
+
+    answer = DFS(n);
+    return answer;
+}
+
 console.log(solution(6));
